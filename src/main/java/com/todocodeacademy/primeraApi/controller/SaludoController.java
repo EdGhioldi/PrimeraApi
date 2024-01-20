719a5c3766db72bb2e2b963b46861466df01c042
EduardoGhioldi <ghioldigahonaeduardo@gmail.com>
2024-01-20 03:10:29 +0000

    Api Finalizada

---
+++ src/main/java/com/todocodeacademy/primeraApi/controller/SaludoController.java
@@ -0,0 +1,21 @@
+    package com.todocodeacademy.primeraApi.controller;
+
+    import org.springframework.web.bind.annotation.GetMapping;
+    import org.springframework.web.bind.annotation.PathVariable;
+    import org.springframework.web.bind.annotation.RequestMapping;
+    import org.springframework.web.bind.annotation.RestController;
+
+    @RestController
+    @RequestMapping("/apisaludos")
+    public class SaludoController {
+
+        @GetMapping("/hola")
+        public String holaMundo(){
+            return "Hola Mundo";
+        }
+        @GetMapping("/holanombre/{nombre}/{edad}")
+        public String holaMundoNombre(@PathVariable String nombre, 
+                                      @PathVariable int edad){
+            return "Hola mi bella " + nombre + " , tu edad es: " + edad;
+        }
+    }
